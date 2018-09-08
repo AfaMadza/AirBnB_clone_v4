4bb4e89ba3de3911ca608513c71fc46beebac404
MitaliSengupta <mitali.s.auger@gmail.com>
2018-09-08 21:20:30 +0000

    adding jscript to check uncheck boxes and list the checked in h4 tag + fixing css

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,11 @@
 let storing = {};
-window.onload(function () {
-  $("input[type=checkbox]
+window.onload = (function () {
+  $("input[type=checkbox]").click(function () {
+    if(this.checked) {
+      storing[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete storing[$(this).data('id')];
+    }
+    $(".amenities h4").text(Object.values(storing).join(', '));
+  });
+});
