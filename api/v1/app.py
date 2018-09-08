b0a8b05b88c1d7ba4bae19d5fe0e4233183afd72
Afa Madza <305@holbertonschool.com>
2018-09-08 23:34:13 +0000

    Changed CORS and updated routes

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,6 +24,7 @@ port = os.getenv('HBNB_API_PORT', 5000)
 # Cross-Origin Resource Sharing
 cors = CORS(app, resources={r'/*': {'origins': host}})
 
+
 # app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
 
