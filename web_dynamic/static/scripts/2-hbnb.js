e615010cf6f4c1932b5dede9b34c9a711bffe9a9
MitaliSengupta <mitali.s.auger@gmail.com>
2018-09-09 01:52:24 +0000

    making sure the popup does not scroll up when deleted

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -4,10 +4,15 @@ window.onload = (function () {
   $("input[type=checkbox]").click(function () {
     if(this.checked) {
       storing[$(this).data('id')] = $(this).data('name');
+      $(".amenities h4").text(Object.values(storing).join(', '));
     } else {
       delete storing[$(this).data('id')];
-    }
-    $(".amenities h4").text(Object.values(storing).join(', '));
+      if (Object.values(storing).length === 0) {
+	$('.amenities h4').html("&nbsp;");
+	} else {
+	  $(".amenities h4").text(Object.values(storing).join(', '));
+	  }
+      }
   });
   $.get(url, function (data, status) {
     if (status === 'success') {
