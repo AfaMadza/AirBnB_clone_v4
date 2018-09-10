23c9b1b0452d2a60dc5f9b64eac6122c12fff2ff
Afa Madza <305@holbertonschool.com>
2018-09-10 21:09:41 +0000

    Changed localhost to 0.0.0.0

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,5 +1,5 @@
 let storing = {};
-const url = 'http://localhost:5001/api/v1/status/';
+const url = 'http://0.0.0.0:5001/api/v1/status/';
 window.onload = (function () {
   $("input[type=checkbox]").click(function () {
     if(this.checked) {
@@ -22,7 +22,7 @@ window.onload = (function () {
   });
   $.ajax({
     type: "POST",
-    url: "http://localhost:5001/api/v1/places_search/",
+    url: "http://0.0.0.0:5001/api/v1/places_search/",
     dataType: "json",
     contentType: "application/json",
     data: '{}',
@@ -55,7 +55,7 @@ window.onload = (function () {
     $('.places article').remove();
     $.ajax({
       type: "POST",
-      url: "http://localhost:5001/api/v1/places_search/",
+      url: "http://0.0.0.0:5001/api/v1/places_search/",
       dataType: "json",
       contentType: "application/json",
       data: JSON.stringify({"amenities": Object.keys(storing)}),
